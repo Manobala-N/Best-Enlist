6c09b32459ec5f15c031f33aca007417241008fd
Manobala-N <85210746+Manobala-N@users.noreply.github.com>
2021-06-14 00:54:34 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,73 @@
+//Indexing Using String 
+
+Days = "Best Enlist"
+print(Days[3])
+t
+
+//Printing the particular character from certain 
+
+word = "Best Enlist"
+print(word[5:11])
+Enlist
+
+//Length of Character
+
+a="Best Enlist"
+print(len(Challenge))
+11
+
+//Converting String into lower character
+ 
+a= "BEST ENLIST"
+print(a.lower())
+best enlist
+
+//String Concatenation – Joining two strings
+
+a = "Best"
+b = "Enlist"
+c = a + b
+print(c)
+BestEnlist
+
+//Adding space during concatenation
+
+a = "Best"
+b = "Enlist"
+c = a + " " + b
+print(c)
+Best Enlist
+
+//casefold() - Usage
+a = "Best Enlist"
+x = a.casefold()
+print(x)
+best enlist
+
+a = "Best Enlist"
+x = a.capitalize()
+print(x)
+Best enlist
+
+a = "Best Enlist"
+x = a.isalpha()
+print(x)
+False
+
+a = "Best Enlist"
+x = a.isalnum()
+print(x)
+False
+
+
+
+
+
+
+
+
+
+
+
+
+
