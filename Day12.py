5c44752cbbfee735b5050dcc3889df977f6b1398
Manobala-N <85210746+Manobala-N@users.noreply.github.com>
2021-06-24 23:32:15 +0530

    Add files via upload

---
+++ Day12.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+f =open("30_days_30_hour_operations.txt","w+")
+f.write("I have completed 10 days successfully")
+f.close()
+f1 =open("30_days_30_hour_operations.txt","a+") 
+f1.writelines(" Manobala.N")
+f1 =open("30_days_30_hour_operations.txt","r") 
+print(f1.read())
+f1.close()
+
